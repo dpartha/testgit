9fd49641badeced90101b2b1ef466c65869d6f7b
Partha Pratim Das <dasparthapratim8274@gmail.com>
2022-11-13 19:49:59 +0530

    Adding new file

---
+++ div.java
@@ -0,0 +1,9 @@
+public class divOfNumber 
+{  
+    public static void main(String args[])   
+    {  
+        int n1 = 225, n2 = 115, div;  
+        div = n1 - n2;  
+        System.out.println("The div: "+div);  
+    }  
+}
