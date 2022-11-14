d4405c7525a5b2b7d72ddc96f06cc78ff8a2cb61
Partha Pratim Das <dasparthapratim8274@gmail.com>
2022-11-14 16:17:55 +0530

    Change in div.java File

--- div.java
+++ div.java
@@ -2,8 +2,6 @@ public class divOfNumber
 {  
     public static void main(String args[])   
     {  
-        int n1 = 225, n2 = 115, div;  
-        div = n1 - n2;  
-        System.out.println("The div of two no: "+div);  
+        
     }  
 }
