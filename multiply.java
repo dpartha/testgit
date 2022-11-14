d3a3cb6e2322ccba447fe5726ecbc28036699bf4
Partha Pratim Das <dasparthapratim8274@gmail.com>
2022-11-14 16:04:49 +0530

    multiply features be added

---
+++ multiply.java
@@ -0,0 +1,9 @@
+public class multiplyOfNumber 
+{  
+    public static void main(String args[])   
+    {  
+        int n1 = 2, n2 = 5, mul;  
+        mul = n1 - n2;  
+        System.out.println("The mul of two no: "+mul);  
+    }  
+}
